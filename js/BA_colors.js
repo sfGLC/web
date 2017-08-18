3b2289d6666bdaf806c0217d5baf06ccfe9302f8
sfGLC <sfan@glc.org>
2017-08-18 16:09:28 -0400

    CSS control visual elements

---
+++ js/BA_colors.js
@@ -0,0 +1,9 @@
+function getColor(r, g, b){
+    return 'rgb(' + r + ',' + g +',' + b +')';
+}
+
+var ColorPicker = {
+    blue1: getColor(28, 45, 90),
+    blue2: getColor(175, 223, 228),
+    red1: getColor(255, 0, 0)
+};
