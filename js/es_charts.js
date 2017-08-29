630cb9d80af012af3e2bcb954c56af874f5342df
sfGLC <sfan@glc.org>
2017-08-29 14:03:47 -0400

    Generate 10 charts. Abandon es_data.js

--- js/es_charts.js
+++ js/es_charts.js
@@ -96,12 +96,12 @@ function createWatershedChart(eleID, name, filename, fraction, chartType, target
             series: []
         },
         TPloading_S: {
-            name: 'Spring TP Loading',
+            name: 'Spring TP Loads',
             unit: 'Metric Tons',
             series: []
         },
         DRPloading_S: {
-            name: 'Spring DRP Loading',
+            name: 'Spring DRP Loads',
             unit: 'Metric Tons',
             series: []
         },
@@ -123,37 +123,54 @@ function createWatershedChart(eleID, name, filename, fraction, chartType, target
         addToFraction: function(name, year, val){
             switch (name){
                 case this.TPloading.name:
-                    this.TPloading.series.splice(year - 2008, 0, parseFloat(val));
+                    this.TPloading.series.splice(year - 2008, 0, (val));
                     break;
                 case this.DRPloading.name:
-                    this.DRPloading.series.splice(year - 2008, 0, parseFloat(val));
+                    this.DRPloading.series.splice(year - 2008, 0, (val));
                     break;
                 case this.TPFWMC.name:
-                    this.TPFWMC.series.splice(year - 2008, 0, parseFloat(val));
+                    this.TPFWMC.series.splice(year - 2008, 0, (val));
                     break;
                 case this.DRPFWMC.name:
-                    this.DRPFWMC.series.splice(year - 2008, 0, parseFloat(val));
+                    this.DRPFWMC.series.splice(year - 2008, 0, (val));
                     break;
                 case this.discharge.name:
-                    this.discharge.series.splice(year - 2008, 0, parseFloat(val));
+                    this.discharge.series.splice(year - 2008, 0, (val));
                     break;
+                case this.TPloading_S.name:
+                    this.TPloading_S.series.splice(year - 2008, 0, val);
+                    break;
+                case this.DRPloading_S.name:
+                    this.DRPloading_S.series.splice(year - 2008, 0, val);
+                    break;
+                case this.TPFWMC_S.name:
+                    this.TPFWMC_S.series.splice(year - 2008, 0, val);
+                    break;
+                case this.DRPFWMC_S.name:
+                    this.DRPFWMC_S.series.splice(year - 2008, 0, val);
+                    break;
+                case this.discharge_S.name:
+                    this.discharge_S.series.splice(year - 2008, 0, val);
+                    break;
+                default:
+                    alert(name + ' ' + year + ' ' + val);
             }
         }
     };
 
     d3.csv(filename, function (in_file){
         in_file.forEach(function (obj){
-            watershed.addToFraction(obj.Fraction, parseInt(obj.Year), obj.Value);
+            watershed.addToFraction(obj.Fraction, parseInt(obj.Year), parseFloat(obj.Value));
         });
         var chart;
         switch (chartType) {
             case 'line':
                 // chart = createLineChart(eleID, watershed[fraction].series, watershed[fraction].name, watershed[fraction].unit, watershed[fraction].name, watershed[fraction].unit, ColorPicker.blue1);
-                chart = createLineChart2(eleID, watershed.name, watershed[fraction], ColorPicker.blue1);
+                chart = createLineChart2(eleID, watershed.name, watershed[fraction], ColorPicker.blue7);
                 break;
             case 'column':
                 // chart = createColumnChart(eleID, watershed[fraction].series, watershed[fraction].name, watershed[fraction].unit, watershed[fraction].name, watershed[fraction].unit, ColorPicker.blue2);
-                chart = createColumnChart2(eleID, watershed.name, watershed[fraction], ColorPicker.blue2);
+                chart = createColumnChart2(eleID, watershed.name, watershed[fraction], ColorPicker.blue7);
                 break;
             case 'column2':
                 chart = createColumnChart2(eleID, watershed.name, watershed[fraction], ColorPicker.blue3);
@@ -232,7 +249,7 @@ function createLineChart2(eleId, w_name, data, color) {
             height: chart_height
         },
         title: {
-            text: w_name + " " + data.name
+            text: w_name + " River " + data.name
         },
         credits: {
             enabled: false
@@ -290,7 +307,7 @@ function createColumnChart2(eleId, w_name, data, color) {
             height: chart_height
         },
         title: {
-            text: w_name + " " + data.name
+            text: w_name + " River " + data.name
         },
         credits: {
             enabled: false
